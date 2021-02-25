1b06cbeb514d7e1c9c8a76c54d31033b3e2d0cb0
Sultana Tasnim Jahan <tasnim047sultana@gmail.com>
2021-02-25 11:18:11 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>My revision</p>
+        <Counter></Counter>
+        <Users></Users>
+        <Actors></Actors>
+        
       </header>
     </div>
   );
+}
+
+function Counter() {
+  const [count,setCount]=useState(0);
+  // const handleDecrease = ()=>setCount(count-1)
+  
+  return (
+    <div>
+      <h2>Count:{count}</h2>
+      <button onClick={()=>setCount(count-1)}>decrease</button>
+      <button onClick={()=>setCount(count+1)}>increase</button>
+    </div>
+  )
+}
+
+
+function Users() {
+  const [users,setUser]=useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUser(data))
+  },[])
+  console.log(users);
+  return(
+    <div>
+      <h2>Dynamic Users:{users.length}</h2>
+      <ul>
+        {
+          users.map(user=><li>{user.name}</li>)
+        }
+      </ul>
+    </div>
+  )
+}
+
+
+
+function Actors(props) {
+  const nayoks=['Mannna','Shakib','Purnima','Ria'];
+  // const nayokName=nayoks.map(nayok=>nayok)
+  // console.log(nayokName);
+  return(
+    <div>
+      <h3>Actors are:</h3>
+      <ul>
+        {
+          nayoks.map(nayok=><li>{nayok}</li>)
+        }
+      </ul>
+    </div>
+  )
 }
 
 export default App;
